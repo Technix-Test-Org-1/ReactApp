4b85ad79abb79afd84a8e387858b5f7ed85e706a
Arvindkumar Sunilkumar <arvindkumar.sunilkumar@msc.com>
2024-07-15 18:37:14 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import { Button } from '@progress/kendo-react-buttons';
+import { ComboBox } from '@progress/kendo-react-dropdowns';
+import '@progress/kendo-theme-default/dist/all.css';
 
 function App() {
+    function Samplefun(e) {
+        e.preventDefault();
+        alert("Helloooo");
+    }
   return (
     <div className="App">
-      <h1> Hello World!</h1>
+          <h1> Hello World!</h1>
+          <Button onClick={Samplefun}> Hello</Button>
+          <ComboBox
+              className="Appbtn"
+              data={['ABC', 'DEF', 'GHI']}
+              placeholder="Select an option"
+          />
     </div>
   );
 }
